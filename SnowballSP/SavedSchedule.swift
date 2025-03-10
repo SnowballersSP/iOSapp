b0e11fd2082b7f0ce7500e1f05111f7134fcfa2b
NBuckle <nikolasmbuckle@gmail.com>
2025-03-10 18:59:33 -0400

    Added weather api, and edit & delete functionalities to saved schedules

---
+++ SnowballSP/SavedSchedule.swift
@@ -0,0 +1,18 @@
+//
+//  SavedSchedule.swift
+//  SnowballSP
+//
+//  Created by Brianna John on 3/10/25.
+//
+
+import SwiftUI
+
+struct SavedSchedule: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SavedSchedule()
+}
